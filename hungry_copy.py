a891efb0a3d62bc66d275a341285ba8cf17ec7e6
Mrinalika <mrinalika2905@gmail.com>
2024-01-29 03:45:14 +0530

    thirsty

---
+++ hungry_copy.py
@@ -0,0 +1,9 @@
+hungry = input("Are you hungry?")
+if hungry == "yes":
+    print("Eat Samosa")
+    print("Eat pizza")
+    print("Eat burger")
+else:
+    thirsty = input("Are you thirsty?")
+    if thirsty == "yes":
+        print("Drink Water")
